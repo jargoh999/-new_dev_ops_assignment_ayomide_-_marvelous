54fb042657ba57bbbff62d9a20d496ce1f510b56
jargoh999 <fatoyeayomide123456@gmail.com>
2024-08-26 22:42:04 +0100

    Initial commit

---
+++ src/test/java/com/devops/assignment/devops_assignment/DevopsAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devops.assignment.devops_assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
