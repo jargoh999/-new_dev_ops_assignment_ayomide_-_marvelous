24e9e570d700597d9cae594d2b8cf246fe49dbc0
jargoh999 <fatoyeayomide123456@gmail.com>
2024-08-26 23:39:59 +0100

    i committed

---
+++ src/main/java/com/devops/assignment/devops_assignment/dtos/RegisterRequest.java
@@ -0,0 +1,2 @@
+package com.devops.assignment.devops_assignment.dtos;public class RegisterRequest {
+}
---
+++ src/main/java/com/devops/assignment/devops_assignment/services/UserServices.java
@@ -0,0 +1,2 @@
+package com.devops.assignment.devops_assignment.services;public interface UserServices {
+}
