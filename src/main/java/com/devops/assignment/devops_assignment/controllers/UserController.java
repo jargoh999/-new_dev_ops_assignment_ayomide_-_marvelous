4fa54db749fa82040c43577022b42ab56b144826
jargoh999 <fatoyeayomide123456@gmail.com>
2024-08-27 11:43:50 +0100

    added controllers

---
+++ src/main/java/com/devops/assignment/devops_assignment/controllers/UserController.java
@@ -0,0 +1,37 @@
+package com.devops.assignment.devops_assignment.controllers;
+
+import com.devops.assignment.devops_assignment.dtos.LoginRequest;
+import com.devops.assignment.devops_assignment.dtos.RegisterRequest;
+import com.devops.assignment.devops_assignment.services.impl.UserServicesImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import static org.springframework.http.HttpStatus.OK;
+
+@Controller
+@RequestMapping("/users")
+public class UserController {
+    @Autowired
+    private UserServicesImpl userServices;
+   @PostMapping("/register")
+    public ResponseEntity<?> register(@RequestBody RegisterRequest registerRequest){
+        try {
+            return new ResponseEntity<>(userServices.registerUser(registerRequest), OK);
+        }catch ( Exception e ) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+        }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody LoginRequest registerRequest){
+        try {
+            return new ResponseEntity<>(userServices.logIn(registerRequest), OK);
+        }catch ( Exception e ) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+}
