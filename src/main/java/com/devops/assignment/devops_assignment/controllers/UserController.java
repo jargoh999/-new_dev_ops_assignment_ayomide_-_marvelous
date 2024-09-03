7c54d3fb1e8adafb189241d88da4dd82dc10fd19
jargoh999 <fatoyeayomide123456@gmail.com>
2024-09-03 15:14:04 +0100

    commit

--- src/main/java/com/devops/assignment/devops_assignment/controllers/UserController.java
+++ src/main/java/com/devops/assignment/devops_assignment/controllers/UserController.java
@@ -6,6 +6,7 @@ import com.devops.assignment.devops_assignment.services.impl.UserServicesImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -33,5 +34,10 @@ public class UserController {
         }catch ( Exception e ) {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
+    }
+
+    @GetMapping("/hello")
+    public String hello(){
+       return "hello";
     }
 }
